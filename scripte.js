af42ca11bbfc26717c466159cdd1ee3660e89333
sarah almutari <sara0moha78@gmail.com>
2022-08-21 14:19:32 +0300

    .

---
+++ scripte.js
@@ -0,0 +1,6 @@
+let contactUs = document.getElementById(`contactus`)
+let searchValue = document.getElementById(`inputsearch`)
+
+function Submit(){
+    contactUs.innerHTML = `<h1>Thank You For Contacting Us!</h1>`
+}
