45ffa4ed0138d4aab01c1e2a7c416f26060b0a42
sarah almutari <sara0moha78@gmail.com>
2022-08-23 21:15:56 +0300

    .

--- scripte.js
+++ scripte.js
@@ -13,44 +13,7 @@ function Submit(){
 
 
 
-
-//products
-let mystery = [` A Good Girl's Guide to Murder`,` A Forgotten Murder`,` Murder on the Orient Express`]
-let classics = [ `The Great Gatsby`,`The Count of Monte Cristo`, `The Catcher in the Rye`] 
-let fantasy = [`Six of Crow`, `The Lord of the Rings`,`Harry Potter and the Half-Blood Prince`]
-let mysterybookname = document.getElementById(`mysterybookname`)
-let classicsbookname = document.getElementById(`classicsbookname`)
-let fantasybookname = document.getElementById(`fantasybookname`)
-
-
-mystery.forEach(x => {
-    mysterybookname.innerHTML += `      
-  <div class="box">
-        <h1>${x}</h1>
-        <img src="${x}" alt="book" width="200px">
-        <h4>price</h4>
-        <button id = "cartbtn" class ="btn" onclick = "btnc()">Add To Cart</button>
-    </div>`
-});
-classics.forEach(x => {
-    classicsbookname.innerHTML += `      
-  <div class="box">
-        <h1>${x}</h1>
-        <img src="${x}" alt="book" width="200px">
-        <h4>price</h4>
-        <button id = "cartbtn" class ="btn">Add To Cart</button>
-    </div>`
-});
-fantasy.forEach(x => {
-    fantasybookname.innerHTML += `      
-  <div class="box">
-        <h1>${x}</h1>
-        <img src="${x}" alt="book" width="200px">
-        <h4>price</h4>
-        <button id = "cartbtn" class ="btn">Add To Cart</button>
-    </div>`
-});
-
+//button
 let cartbtn = document.getElementById(`cartbtn`)
  let shopbox = document.getElementById(`shopbox`)
 function btnc() {
