60afa6a5d77b8557cb59ac7eafb8599d8f5e88b5
sarah almutari <sara0moha78@gmail.com>
2022-08-24 19:02:54 +0300

    .

--- scripte.js
+++ scripte.js
@@ -12,6 +12,82 @@ function Submit(){
 }
 
 
+//products
+// let mystery = [`A Good Girl's Guide to Murder`,` A Forgotten Murder`,` Murder on the Orient Express`]
+// let classics = [ `The Great Gatsby`,`Pride and Prejudice`, `The Catcher in the Rye`] 
+// let fantasy = [`Six of Crow`, `The Lord of the Rings`,`Harry Potter and the Half-Blood Prince`]
+
+let mystery = [
+{name:`A Good Girl's Guide to Murder`, img:``, price:``,btn:``},
+ {name:`A Forgotten Murder`, img:``, price:``,btn:``},
+ {name:`Murder on the Orient Express`, img:``, price:``,btn:``},
+]
+let classics = [ 
+     {name:`The Great Gatsby`, img:``, price:``,btn:``},
+      {name:`Pride and Prejudice`, img:``, price:``,btn:``},
+       {name:`The Catcher in the Rye`, img:``, price:``,btn:``},
+]
+let fantasy = [
+    
+ {name:`Six of Crow`, img:``, price:``,btn:``},
+  {name:`The Lord of the Rings`, img:``, price:``,btn:``},
+   {name:`Harry Potter and the Half-Blood Prince`, img:``, price:``,btn:``},
+]
+let mysterybookname = document.getElementById(`mysterybookname`)
+let classicsbookname = document.getElementById(`classicsbookname`)
+let fantasybookname = document.getElementById(`fantasybookname`)
+
+
+
+
+
+
+
+
+
+
+
+mystery.forEach(x => {
+    mysterybookname.innerHTML += `      
+  <div class="box">
+        <h1>${x}</h1>
+        <img src="${x}" alt="book" width="200px">
+        <h4>price</h4>
+        <button id = "cartbtn" class ="btn" onclick = "btnc()">Add To Cart</button>
+    </div>`
+});
+classics.forEach(x => {
+    classicsbookname.innerHTML += `      
+  <div class="box">
+        <h1>${x}</h1>
+        <img src="${x}" alt="book" width="200px">
+        <h4>price</h4>
+        <button id = "cartbtn" class ="btn">Add To Cart</button>
+    </div>`
+});
+fantasy.forEach(x => {
+    fantasybookname.innerHTML += `      
+  <div class="box">
+        <h1>${x}</h1>
+        <img src="${x}" alt="book" width="200px">
+        <h4>price</h4>
+        <button id = "cartbtn" class ="btn">Add To Cart</button>
+    </div>`
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //button
 let cartbtn = document.getElementsByClassName(`cartbtn`)
